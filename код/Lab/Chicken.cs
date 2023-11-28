dd5600090ae6237d5e9d40bcf3e6055c508c1dd7
korbog <kor.bog.2003@gmail.com>
2023-11-28 11:30:57 +0200

    spr4

---
+++ код/Lab/Chicken.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab
+{
+    public class Chicken : Animal
+    {
+        public Chicken(string name) : base(name)
+        {
+            Console.WriteLine($"Chicken constructor ({name})");
+        }
+
+        public override void AnimalSound()
+        {
+            Console.WriteLine("Coo-coo-coo-coo-coo!");
+        }
+
+        ~Chicken()
+        {
+            Console.WriteLine("Chicken destructor");
+        }
+    }
+}
---
+++ код/Lab/Lab/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab
+{
+    class Program
+    {
+        static void Main()
+        {
+            var animals = new List<Animal>() { 
+                new Cow("Noel"), 
+                new Chicken("Subaru")
+            };
+
+            foreach (var animal in animals)
+            {
+                animal.AnimalSound();
+            }
+        }
+    }
+}
