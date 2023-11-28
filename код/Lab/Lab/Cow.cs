6a6c0ac2b53b8b8f66b4b39d8e3047ffc7412e2f
korbog <kor.bog.2003@gmail.com>
2023-11-28 11:09:25 +0200

    spr4

---
+++ код/Lab/Lab/Cow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab
+{
+    public class Cow : Animal
+    {
+        public Cow(string name) : base(name) 
+        {
+            Console.WriteLine($"Cow constructor ({name})");
+        }
+
+        public override void AnimalSound()
+        {
+            Console.WriteLine("Moo!!");
+        }
+
+        ~Cow()
+        {
+            Console.WriteLine("Cow destructor");
+        }
+    }
+}
